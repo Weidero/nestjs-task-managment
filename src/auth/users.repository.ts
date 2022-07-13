c43fae67ed90dc4e3bbc0ce199e00182925d91aa
Dariusz Horbal <dhorbal@clearcode.cc>
2022-07-13 10:52:08 +0200

    Type ORM & Auth

---
+++ src/auth/users.repository.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dtos/auth-credentials.dto';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersRepository {
+  constructor(
+    @InjectRepository(User)
+    private readonly userEntityRepository: Repository<User>,
+  ) {}
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.userEntityRepository.create({
+      username,
+      password,
+    });
+    await this.userEntityRepository.save(user);
+  }
+}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksController } from 'src/tasks/tasks.controller';
 import { TasksService } from 'src/tasks/tasks.service';
+import { Task } from './task.entity';
 import { TasksRepository } from './tasks.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TasksRepository])],
+  imports: [TypeOrmModule.forFeature([Task])],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [TasksService, TasksRepository],
 })
 export class TasksModule {}
